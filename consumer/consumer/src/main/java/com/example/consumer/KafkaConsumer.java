54e512ac1e0ba3d401453722f5e815fad799fea8
Raunak <rounand44@gmail.com>
2026-05-01 01:11:36 +0530

    new addition

---
+++ consumer/consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.example.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "ms-tpoic",groupId = "ms-group")
+    public void listen(String message)
+    {
+        System.out.println("Message received: "+message);
+    }
+
+    @KafkaListener(topics = "ms-tpoic",groupId = "ms-group-1")
+    public void listen1(String message)
+    {
+        System.out.println("Message received: "+message);
+    }
+}
---
+++ producer/producer/src/main/java/com/example/producer/KafkaProducer.java
@@ -0,0 +1,28 @@
+package com.example.producer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/producer")
+public class KafkaProducer {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message)
+    {
+       kafkaTemplate.send("ms-tpoic",message);
+
+       return "Message sent: "+message;
+    }
+
+}
